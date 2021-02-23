226c14f10cb50592cb1a056fc9e0f65559b38f5a
Jacob Archambault <jacobarchambault@gmail.com>
2021-02-22 22:04:13 -0500

    Rename Program to Main

--- src/com/jacobarchambault/dormcalculator/Main.java
+++ src/com/jacobarchambault/dormcalculator/Main.java
@@ -1,6 +1,6 @@
 package com.jacobarchambault.dormcalculator;
 
-public class Program {
+public class Main {
 	public static void main(
 			String[] args) {
 		new PropertyTaxCalculator("Property taxes");
